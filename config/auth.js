22cced735a8c1cd0af0738c0bd4824703e3ba2e4
Dinah Bakeer <dinah.bakeer@gmail.com>
2015-11-30 01:19:58 -0500

    fixed heroku name and files

--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,6 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1806386009588489',
         'clientSecret'  : 'ccd2a3a98d6d215169a9bacdc952a033',
-        'callbackURL'   : 'http://fr1endr.herokuapp.com/auth/facebook/callback'
+        'callbackURL'   : 'http://dec1bel.herokuapp.com/auth/facebook/callback'
     }
 };
