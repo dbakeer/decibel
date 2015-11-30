7893c363ab3cdec9b6eef41200b58daa77adfdb4
Dinah Bakeer <dinah.bakeer@gmail.com>
2015-11-29 22:09:14 -0500

    fixing api call

--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,6 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1806386009588489',
         'clientSecret'  : 'ccd2a3a98d6d215169a9bacdc952a033',
-        'callbackURL'   : 'http://localhost:3000/auth/facebook/callback'
+        'callbackURL'   : 'http://fr1endr.herokuapp.com/auth/facebook/callback'
     }
 };
